8969b355e07f5684c57bc9ab4174d9e502b158a2
kongfanye <1595621152@qq.com>
2025-06-23 21:18:40 +0800

    Update EmbeddedAssign2.cpp

--- EmbeddedAssign2.cpp
+++ EmbeddedAssign2.cpp
@@ -2,35 +2,41 @@
 const int ledPin1=12;
 const int ledPin2=14;
 
-
-
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-  pinMode(ledPin1,OUTPUT);
-  pinMode(ledPin2,OUTPUT);
-
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
-void loop() {
-  
-  if(Serial.available()>0){
+void loop() 
+{
+  if(Serial.available() > 0)
+{
     String receivedCommand = Serial.readStringUntil('\n');
     receivedCommand.trim();
-    if(receivedCommand == "1"){
+    if(receivedCommand == "1")
+    {
       digitalWrite(ledPin1,HIGH);
       Serial.println("LED1亮");
-
-    }else if(receivedCommand == "2"){
-      digitalWrite(ledPin2,HIGH);
+    }
+    else if(receivedCommand == "2")
+    {
+      digitalWrite(ledPin2, HIGH);
       Serial.println("LED2亮");
-  }else if(receivedCommand == "01"){
-      digitalWrite(ledPin1,LOW);
+    }
+    else if(receivedCommand == "01")
+    {
+      digitalWrite(ledPin1, LOW);
       Serial.println("LED1灭");
     }
-    else if(receivedCommand == "02"){
-      digitalWrite(ledPin2,LOW);
+    else if(receivedCommand == "02")
+    {
+      digitalWrite(ledPin2, LOW);
       Serial.println("LED2灭");
-    }else{
+    }
+    else
+    {
       Serial.println("无效指令");
     }
 }
